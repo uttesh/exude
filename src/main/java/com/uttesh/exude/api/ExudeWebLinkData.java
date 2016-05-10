1f13956b57c6cc670e06102be51e458efb2cf139
androkumar04 <androkumar04@gmail.com>
2016-05-10 07:12:37 +0530

    code cleanup

--- src/main/java/com/uttesh/exude/api/ExudeWebLinkData.java
+++ src/main/java/com/uttesh/exude/api/ExudeWebLinkData.java
@@ -1,3 +1,18 @@
+/*
+ Copyright 2016 Uttesh Kumar T.H.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 package com.uttesh.exude.api;
 
 import com.uttesh.exude.common.ExudeRequest;
@@ -20,7 +35,7 @@ import org.xml.sax.SAXException;
 
 /**
  *
- * @author Rivetlabs
+ * @author Uttesh Kumar T.H.
  */
 public class ExudeWebLinkData implements ExudeAPI {
 
--- src/main/java/com/uttesh/exude/stemming/Stemmer.java
+++ src/main/java/com/uttesh/exude/stemming/Stemmer.java
@@ -1,3 +1,4 @@
+
 package com.uttesh.exude.stemming;
 
 
